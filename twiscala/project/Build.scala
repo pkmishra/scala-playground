d56139f5c3d785bbe3c1be36f56be4c5831035e0
Pradeep Kumar Mishra <pkmishra@gmail.com>
2013-11-26 23:10:34 +0530

    added twiscala project

---
+++ twiscala/project/Build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "twiscala"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    "org.mindrot" % "jbcrypt" % "0.3m",
+    "com.typesafe.slick" %% "slick" % "1.0.1",
+    "com.h2database" % "h2" % "1.3.166"
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
